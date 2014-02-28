ca2ca40c6c0ad0decf9eb8653cd2765083368a70
David Golden <dagolden@cpan.org>
2014-02-28 03:29:41 +0000

    Import of DAGOLDEN/Net-Amazon-Config-0.002 from CPAN.
    gitpan-cpan-distribution: Net-Amazon-Config
    gitpan-cpan-version:      0.002
    gitpan-cpan-path:         DAGOLDEN/Net-Amazon-Config-0.002.tar.gz
    gitpan-cpan-author:       DAGOLDEN
    gitpan-cpan-maturity:     released

--- lib/Net/Amazon/Config.pm
+++ lib/Net/Amazon/Config.pm
@@ -1,22 +1,16 @@
-# Copyright (c) 2010 by David Golden. All rights reserved.
-# Licensed under Apache License, Version 2.0 (the "License").
-# You may not use this file except in compliance with the License.
-# A copy of the License was distributed with this file or you may obtain a 
-# copy of the License from http://www.apache.org/licenses/LICENSE-2.0
-
-package Net::Amazon::Config;
 use strict;
 use warnings;
 
-our $VERSION = '0.001';
-$VERSION = eval $VERSION; ## no critic
+package Net::Amazon::Config;
+# ABSTRACT: Manage Amazon Web Services credentials
+our $VERSION = '0.002'; # VERSION
 
 use Carp ();
-use Config::Tiny ();
+use Config::Tiny 2.12 ();
 use Net::Amazon::Config::Profile ();
-use Params::Validate ();
-use Path::Class ();
-use Object::Tiny qw(
+use Params::Validate 0.91 ();
+use Path::Class 0.17      ();
+use Object::Tiny 1.06 qw(
   config_dir
   config_file
   config_path
@@ -25,170 +19,193 @@ use Object::Tiny qw(
 use constant IS_WIN32 => $^O eq 'MSWin32';
 
 sub _default_dir {
-  my $base = Path::Class::dir(IS_WIN32 ? $ENV{USERPROFILE} : $ENV{HOME});
-  return $base->subdir('.amazon')->absolute->stringify;
+    my $base = Path::Class::dir( IS_WIN32 ? $ENV{USERPROFILE} : $ENV{HOME} );
+    return $base->subdir('.amazon')->absolute->stringify;
 }
 
 sub new {
-  my $class = shift;
-  my %args = Params::Validate::validate( @_, {
-    config_dir => {
-      default => $ENV{NET_AMAZON_CONFIG_DIR} || _default_dir,
-    },
-    config_file => {
-      default => $ENV{NET_AMAZON_CONFIG} || 'profiles.conf',
+    my $class = shift;
+    my %args  = Params::Validate::validate(
+        @_,
+        {
+            config_dir  => { default => $ENV{NET_AMAZON_CONFIG_DIR} || _default_dir, },
+            config_file => { default => $ENV{NET_AMAZON_CONFIG}     || 'profiles.conf', }
+        }
+    );
+
+    if ( Path::Class::file( $args{config_file} )->is_absolute ) {
+        $args{config_path} = $args{config_file};
     }
-  });
+    else {
+        $args{config_path} =
+          Path::Class::dir( $args{config_dir} )->file( $args{config_file} );
+    }
 
-  if ( Path::Class::file($args{config_file})->is_absolute ) {
-    $args{config_path} = $args{config_file};
-  }
-  else {
-    $args{config_path} = 
-      Path::Class::dir($args{config_dir})->file($args{config_file});
-  }
-  
-  unless ( -r $args{config_path} ) {
-    die "Could not find readable file $args{config_path}";
-  }
+    unless ( -r $args{config_path} ) {
+        die "Could not find readable file $args{config_path}";
+    }
 
-  return bless \%args, $class;
+    return bless \%args, $class;
 }
 
 sub get_profile {
-  my ($self, $profile_name) = @_; 
-  my $config = Config::Tiny->read( $self->config_path );
-  
-  $profile_name = $config->{_}{default} unless defined $profile_name;
-  my $params = $config->{$profile_name}
-    or return;
+    my ( $self, $profile_name ) = @_;
+    my $config = Config::Tiny->read( $self->config_path );
 
-  $params->{profile_name} = $profile_name;
-  my $profile = eval { Net::Amazon::Config::Profile->new( $params ) };
-  if ($@) {
-    Carp::croak "Invalid profile: $@";
-  }
-  return $profile;
+    $profile_name = $config->{_}{default} unless defined $profile_name;
+    my $params = $config->{$profile_name}
+      or return;
+
+    $params->{profile_name} = $profile_name;
+    my $profile = eval { Net::Amazon::Config::Profile->new($params) };
+    if ($@) {
+        Carp::croak "Invalid profile: $@";
+    }
+    return $profile;
 }
 
 1;
 
 __END__
 
-=begin wikidoc
+=pod
 
-= NAME
+=encoding UTF-8
+
+=head1 NAME
 
 Net::Amazon::Config - Manage Amazon Web Services credentials
 
-= VERSION
+=head1 VERSION
 
-This documentation describes version %%VERSION%%.
+version 0.002
 
-= SYNOPSIS
+=head1 SYNOPSIS
 
-== Example
-    use Net::Amazon::Config;
-    
-    # default location and profile
-    my $profile = Net::Amazon::Config->new->get_profile;
+=head2 Example
 
-    # use access key ID and secret access key with S3 
-    use Net::Amazon::S3;
-    my $s3 = Net::Amazon::S3->new(
-      aws_access_key_id     => $profile->access_key_id,
-      aws_secret_access_key => $profile->secret_access_key,
-    );
-    
-== Config Format
+     use Net::Amazon::Config;
+ 
+     # default location and profile
+     my $profile = Net::Amazon::Config->new->get_profile;
+ 
+     # use access key ID and secret access key with S3 
+     use Net::Amazon::S3;
+     my $s3 = Net::Amazon::S3->new(
+       aws_access_key_id     => $profile->access_key_id,
+       aws_secret_access_key => $profile->secret_access_key,
+     );
 
-  default = johndoe
-  [johndoe]
-  access_key_id = XXXXXXXXXXXXXXXXXXXX
-  secret_access_key = XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-  certificate_file = my-cert.pem
-  private_key_file = my-key.pem
-  ec2_keypair_name = my-ec2-keypair
-  ec2_keypair_file = ec2-private-key.pem
-  aws_account_id = 0123-4567-8901
-  canonical_user_id = <64-character string>
-  
-= DESCRIPTION
+=head2 Config Format
+
+   default = johndoe
+   [johndoe]
+   access_key_id = XXXXXXXXXXXXXXXXXXXX
+   secret_access_key = XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+   certificate_file = my-cert.pem
+   private_key_file = my-key.pem
+   ec2_keypair_name = my-ec2-keypair
+   ec2_keypair_file = ec2-private-key.pem
+   aws_account_id = 0123-4567-8901
+   canonical_user_id = <64-character string>
+
+=head1 DESCRIPTION
 
 This module lets you keep Amazon Web Services credentials in a
 configuration file for use with different tools that need them.
 
-= USAGE
+=head1 USAGE
 
-== new() 
+=head2 new() 
 
-  my $config = Net::Amazon::Config->new( %params );
+   my $config = Net::Amazon::Config->new( %params );
 
-Valid {%params} entries include:
+Valid C<<< %params >>> entries include:
 
-* config_dir -- directory containing the config file
+=over
+
+=item *
+
+config_dir -- directory containing the config file
 (and the default location for other files named in the config file).  
-Defaults to {$HOME/.amazon}
-* config_file -- defaults to {profiles.conf}
+Defaults to C<<< $HOME/.amazon >>>
+
+=item *
+
+config_file -- defaults to C<<< profiles.conf >>>
+
+=back
 
 Returns an object or undef if no config file can be found.
 
-== config_path()
+=head2 config_path()
 
-  my $path = $config->config_path;
+   my $path = $config->config_path;
 
 Returns the absolute path to the configuration file.
 
-== get_profile()
+=head2 get_profile()
 
-  my $profile = $config->get_profile( $name );
+   my $profile = $config->get_profile( $name );
 
-If {$name} is omitted or undefined, returns the profile named in the
-top-level key {default} in the config file. If the profile does not
+If C<<< $name >>> is omitted or undefined, returns the profile named in the
+top-level key C<<< default >>> in the config file. If the profile does not
 exist, get profile returns undef or an empty list.
 
-= ENVIRONMENT
+=head1 ENVIRONMENT
 
-* NET_AMAZON_CONFIG -- absolute path to config file or file name relative
+=over
+
+=item *
+
+NET_AMAZON_CONFIG -- absolute path to config file or file name relative
 to the configuration directory
-* NET_AMAZON_CONFIG_DIR -- configuration directory 
 
-= BUGS
+=item *
 
-Please report any bugs or feature requests using the CPAN Request Tracker  
-web interface at [http://rt.cpan.org/Dist/Display.html?Queue=Net-Amazon-Config]
+NET_AMAZON_CONFIG_DIR -- configuration directory 
 
-When submitting a bug or request, please include a test-file or a patch to an
-existing test-file that illustrates the bug or desired feature.
+=back
 
-= SEE ALSO
+=head1 SEE ALSO
 
-* About AWS Security Credentials: http://tinyurl.com/yh93cjg
+=over
 
-= AUTHOR
+=item *
 
-David A. Golden (DAGOLDEN)
+About AWS Security Credentials: http:E<sol>E<sol>tinyurl.comE<sol>yh93cjg
 
-= COPYRIGHT AND LICENSE
+=back
 
-Copyright (c) 2010 by David A. Golden. All rights reserved.
+=for :stopwords cpan testmatrix url annocpan anno bugtracker rt cpants kwalitee diff irc mailto metadata placeholders metacpan
 
-Licensed under Apache License, Version 2.0 (the "License").
-You may not use this file except in compliance with the License.
-A copy of the License was distributed with this file or you may obtain a 
-copy of the License from http://www.apache.org/licenses/LICENSE-2.0
+=head1 SUPPORT
 
-Files produced as output though the use of this software, shall not be
-considered Derivative Works, but shall be considered the original work of the
-Licensor.
+=head2 Bugs / Feature Requests
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+Please report any bugs or feature requests through the issue tracker
+at L<https://github.com/dagolden/Net-Amazon-Config/issues>.
+You will be notified automatically of any progress on your issue.
 
-=end wikidoc
+=head2 Source Code
+
+This is open source software.  The code repository is available for
+public review and contribution under the terms of the license.
+
+L<https://github.com/dagolden/Net-Amazon-Config>
+
+  git clone https://github.com/dagolden/Net-Amazon-Config.git
+
+=head1 AUTHOR
+
+David Golden <dagolden@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2014 by David Golden.
+
+This is free software, licensed under:
+
+  The Apache License, Version 2.0, January 2004
 
 =cut
-
--- lib/Net/Amazon/Config/Profile.pm
+++ lib/Net/Amazon/Config/Profile.pm
@@ -1,119 +1,136 @@
-# Copyright (c) 2010 by David Golden. All rights reserved.
-# Licensed under Apache License, Version 2.0 (the "License").
-# You may not use this file except in compliance with the License.
-# A copy of the License was distributed with this file or you may obtain a 
-# copy of the License from http://www.apache.org/licenses/LICENSE-2.0
-
-package Net::Amazon::Config::Profile;
 use strict;
 use warnings;
-use Params::Validate ();
 
-our $VERSION = '0.001';
-$VERSION = eval $VERSION; ## no critic
+package Net::Amazon::Config::Profile;
+# ABSTRACT: Amazon credentials for given profile
+our $VERSION = '0.002'; # VERSION
+
+use Params::Validate ();
 
 my @attributes;
 
 BEGIN {
-  @attributes = qw(
-    profile_name
-    access_key_id
-    secret_access_key
-    certificate_file
-    private_key_file
-    ec2_keypair_name
-    ec2_keypair_file
-    cf_keypair_id
-    cf_private_key_file
-    aws_account_id
-    canonical_user_id
-  );
+    @attributes = qw(
+      profile_name
+      access_key_id
+      secret_access_key
+      certificate_file
+      private_key_file
+      ec2_keypair_name
+      ec2_keypair_file
+      cf_keypair_id
+      cf_private_key_file
+      aws_account_id
+      canonical_user_id
+    );
 }
 
 use Object::Tiny @attributes;
 
 sub new {
-  my ($class, $first, @rest) = @_;
-  my @args = ref $first eq 'ARRAY' ? (@$first) : ($first,@rest);
-  my %args = Params::Validate::validate(@args, { map { $_ => 0 } @attributes });
-  return bless \%args, $class;
+    my ( $class, $first, @rest ) = @_;
+    my @args = ref $first eq 'ARRAY' ? (@$first) : ( $first, @rest );
+    my %args = Params::Validate::validate( @args, { map { $_ => 0 } @attributes } );
+    return bless \%args, $class;
 }
 
 1;
 
 __END__
 
-=begin wikidoc
+=pod
 
-= NAME
+=encoding UTF-8
+
+=head1 NAME
 
 Net::Amazon::Config::Profile - Amazon credentials for given profile
 
-= VERSION
+=head1 VERSION
 
-This documentation describes version %%VERSION%%.
+version 0.002
 
-= DESCRIPTION
+=head1 DESCRIPTION
 
 This module defines a simple object representing a 'profile' of
 Amazon Web Services credentials and associated information.
 
-= USAGE
+=head1 USAGE
 
-A profile object is created by [Net::Amazon::Config] based on information
+A profile object is created by L<Net::Amazon::Config> based on information
 in a configuration file.  The object has the following read-only accessors:
 
-* profile_name -- as provided in the configuration file
-* access_key_id -- identifier for REST requests
-* secret_access_key -- used to sign REST requests
-* certificate_file -- path to a file containing identifier for SOAP requests
-* private_key_file -- path to a file containing the key used to sign SOAP requests
-* ec2_keypair_name -- the name used to identify a keypair when launching an EC2 instance
-* ec2_keypair_file -- the private key file used by ssh to connect to an EC2 instance
-* cf_keypair_id -- identifier for CloudFront requests
-* cf_private_key_file -- path to a file containing the key use to sign CloudFront requests
-* aws_account_id -- identifier to share resources (except S3) 
-* canonical_user_id -- identifier to share resources (S3 only)
+=over
+
+=item *
+
+profile_name -- as provided in the configuration file
+
+=item *
+
+access_key_id -- identifier for REST requests
+
+=item *
+
+secret_access_key -- used to sign REST requests
+
+=item *
+
+certificate_file -- path to a file containing identifier for SOAP requests
+
+=item *
+
+private_key_file -- path to a file containing the key used to sign SOAP requests
+
+=item *
+
+ec2_keypair_name -- the name used to identify a keypair when launching an EC2 instance
+
+=item *
+
+ec2_keypair_file -- the private key file used by ssh to connect to an EC2 instance
+
+=item *
+
+cf_keypair_id -- identifier for CloudFront requests
+
+=item *
+
+cf_private_key_file -- path to a file containing the key use to sign CloudFront requests
+
+=item *
+
+aws_account_id -- identifier to share resources (except S3) 
+
+=item *
+
+canonical_user_id -- identifier to share resources (S3 only)
+
+=back
 
 If an attribute is not set in the configuration file, the accessor will
 return undef.
 
-= BUGS
+=head1 SEE ALSO
 
-Please report any bugs or feature requests using the CPAN Request Tracker  
-web interface at [http://rt.cpan.org/Dist/Display.html?Queue=Net-Amazon-Config]
+=over
 
-When submitting a bug or request, please include a test-file or a patch to an
-existing test-file that illustrates the bug or desired feature.
+=item *
 
-= SEE ALSO
+L<Net::Amazon::Config>
 
-* [Net::Amazon::Config]
+=back
 
-= AUTHOR
+=head1 AUTHOR
 
-David A. Golden (DAGOLDEN)
+David Golden <dagolden@cpan.org>
 
-= COPYRIGHT AND LICENSE
+=head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2010 by David A. Golden. All rights reserved.
+This software is Copyright (c) 2014 by David Golden.
 
-Licensed under Apache License, Version 2.0 (the "License").
-You may not use this file except in compliance with the License.
-A copy of the License was distributed with this file or you may obtain a 
-copy of the License from http://www.apache.org/licenses/LICENSE-2.0
+This is free software, licensed under:
 
-Files produced as output though the use of this software, shall not be
-considered Derivative Works, but shall be considered the original work of the
-Licensor.
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-=end wikidoc
+  The Apache License, Version 2.0, January 2004
 
 =cut
-
